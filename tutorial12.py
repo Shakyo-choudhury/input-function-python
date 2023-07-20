b384c6bb3895f8b93f3353550a1c7e0f83dc9bef
Shakyo choudhury <94360466+Shakyo-choudhury@users.noreply.github.com>
2023-07-20 22:23:06 +0530

    Add files via upload

---
+++ tutorial12.py
@@ -0,0 +1,11 @@
+# Ask for user input
+age = input("Enter your age: ")
+
+# Convert age to integer
+age = int(age)
+
+# Perform calculations
+next_year_age = age + 1
+
+# Display the result
+print("Next year, you will be " + str(next_year_age) + " years old.")
